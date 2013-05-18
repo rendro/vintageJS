4a04f461e1e2fff8b4a4c5c594ed36be8196fd38
Robert Fleischmann <rendro87@gmail.com>
2013-05-18 16:08:28 +0100

    added sepia effect and fixed issue with screen layer

--- vintage.js
+++ vintage.js
@@ -26,7 +26,8 @@ var vintageJS = function(originalImage, opts, effect) {
         vignette:   false,
         lighten:    false,
         noise:      false,
-        viewFinder: false
+        viewFinder: false,
+        sepia:      false
       };
 
   applyEffect = function (effect) {
@@ -125,7 +126,9 @@ var vintageJS = function(originalImage, opts, effect) {
     var
     idx,
     j, // for check value loop
+    r,g,b, // for sepia
     average,
+    sepiatone,
     noise,
     _imageData = imageData.data;
 
@@ -147,7 +150,7 @@ var vintageJS = function(originalImage, opts, effect) {
       }
 
       // screen
-      if (!!effect.screen && !!effect.screen.r && !!effect.screen.g && !!effect.screen.b && !!effect.screen.a) {
+      if (!!effect.screen) {
         _imageData[idx  ] = 255 - ((255 - _imageData[idx  ]) * (255 - effect.screen.r * effect.screen.a) / 255);
         _imageData[idx+1] = 255 - ((255 - _imageData[idx+1]) * (255 - effect.screen.g * effect.screen.a) / 255);
         _imageData[idx+2] = 255 - ((255 - _imageData[idx+2]) * (255 - effect.screen.b * effect.screen.a) / 255);
@@ -168,8 +171,18 @@ var vintageJS = function(originalImage, opts, effect) {
         _imageData[idx+2] = _imageData[idx+2] * viewFinderImageData[idx+2] / 255;
       }
 
+      // sepia
+      if (!!effect.sepia) {
+        r = _imageData[idx  ];
+        g = _imageData[idx+1];
+        b = _imageData[idx+2];
+        _imageData[idx  ] = r * 0.393 + g * 0.769 + b * 0.189;
+        _imageData[idx+1] = r * 0.349 + g * 0.686 + b * 0.168;
+        _imageData[idx+2] = r * 0.272 + g * 0.534 + b * 0.131;
+      }
+
       // desaturate
-      if (!!effect.desaturate) {
+      if (!!effect.sepia) {
         average = ( _imageData[idx  ] + _imageData[idx+1] + _imageData[idx+2] ) / 3;
         _imageData[idx  ] += ((average - _imageData[idx  ]) * effect.desaturate);
         _imageData[idx+1] += ((average - _imageData[idx+1]) * effect.desaturate);
@@ -182,10 +195,6 @@ var vintageJS = function(originalImage, opts, effect) {
       for (j=2; j>=0; --j) {
         _imageData[idx+j] = ~~(_imageData[idx+j] > 255 ? 255 : _imageData[idx+j] < 0 ? 0 : _imageData[idx+j]);
       }
-
-      // if (i <= 0) {
-
-      // }
     }
     // write image data, finalize vintageJS
     ctx.putImageData(imageData, 0, 0);
