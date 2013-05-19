fa020ea3ddfca6942220fa40983618d3bed0442a
Robert Fleischmann <rendro87@gmail.com>
2013-05-19 13:22:18 +0100

    refractor code and restyle download button

---
+++ assets/scripts/slideshow.js
@@ -0,0 +1,20 @@
+var Slideshow = function($node) {
+  var $items  = $node.find('li'),
+      currIdx = 0,
+      active  = 'active',
+      click   = 'click',
+      max     = $items.length-1,
+      slide   = function(back) {
+        $items.eq( back ? (currIdx-1 < 0 ? max : --currIdx) : (currIdx+1 > max ? 0 : ++currIdx)).trigger(click);
+      };
+  $node.on(click, 'li', function() {
+    var $this = $(this);
+    if ($this.hasClass(active)) { return; }
+    $this.siblings( ['.',active].join('') ).addBack().toggleClass('active');
+    currIdx = $this.index();
+  }).on(click, '.js_next', function() {
+    slide();
+  }).on(click, '.js_prev', function() {
+    slide(true);
+  });
+};
---
+++ assets/scripts/vintage.presets.js
@@ -0,0 +1,104 @@
+var vintagePresets = {
+  /**
+   * Basic vintage effect
+   */
+  vintage: {
+    curves: (function() {
+      var rgb = function (x) {
+        return -12 * Math.sin( x * 2 * Math.PI / 255 ) + x;
+      },
+      r = function(x) {
+        return -0.2 * Math.pow(255 * x, 0.5) * Math.sin(Math.PI * (-0.0000195 * Math.pow(x, 2) + 0.0125 * x ) ) + x;
+      },
+      g = function(x) {
+        return -0.001045244139166791 * Math.pow(x,2) + 1.2665372554875318 * x;
+      },
+      b = function(x) {
+        return 0.57254902 * x + 53;
+      },
+      c = {r:[],g:[],b:[]};
+      for(var i=0;i<=255;++i) {
+        c.r[i] = r( rgb(i) );
+        c.g[i] = g( rgb(i) );
+        c.b[i] = b( rgb(i) );
+      }
+      return c;
+    })(),
+    screen: {
+      r: 227,
+      g: 12,
+      b: 169,
+      a: 0.15
+    },
+    vignette: 0.7,
+    viewFinder: 'img/viewfinder.jpg'
+  },
+  /**
+   * Sepia effect
+   */
+  sepia: {
+    curves: (function() {
+      var rgb = function (x) {
+        return -12 * Math.sin( x * 2 * Math.PI / 255 ) + x;
+      },
+      c = {r:[],g:[],b:[]};
+      for(var i=0;i<=255;++i) {
+        c.r[i] = rgb(i);
+        c.g[i] = rgb(i);
+        c.b[i] = rgb(i);
+      }
+      return c;
+    })(),
+    sepia: true
+  },
+  /**
+   * Greenish effect
+   */
+  greenish: {
+    curves: (function() {
+      var rgb = function (x) {
+        return -12 * Math.sin( x * 2 * Math.PI / 255 ) + x;
+      },
+      c = {r:[],g:[],b:[]};
+      for(var i=0;i<=255;++i) {
+        c.r[i] = rgb(i);
+        c.g[i] = rgb(i);
+        c.b[i] = rgb(i);
+      }
+      return c;
+    })(),
+    vignette: 0.6,
+    lighten: 0.1,
+    screen: {
+      r: 255,
+      g: 255,
+      b: 0,
+      a: 0.15
+    }
+  },
+  /**
+   * Reddish effect
+   */
+  reddish: {
+    curves: (function() {
+      var rgb = function (x) {
+        return -12 * Math.sin( x * 2 * Math.PI / 255 ) + x;
+      },
+      c = {r:[],g:[],b:[]};
+      for(var i=0;i<=255;++i) {
+        c.r[i] = rgb(i);
+        c.g[i] = rgb(i);
+        c.b[i] = rgb(i);
+      }
+      return c;
+    })(),
+    vignette: 0.6,
+    lighten: 0.1,
+    screen: {
+      r: 255,
+      g: 0,
+      b: 0,
+      a: 0.15
+    }
+  }
+};
