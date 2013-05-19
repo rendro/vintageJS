e92fcd361ddc098314f5f3113671ab40856de0a0
Robert Fleischmann <rendro87@gmail.com>
2013-05-19 15:34:28 +0100

    add slideshow:change event and an api to get the current slide

--- assets/scripts/slideshow.js
+++ assets/scripts/slideshow.js
@@ -7,14 +7,22 @@ var Slideshow = function($node) {
       slide   = function(back) {
         $items.eq( back ? (currIdx-1 < 0 ? max : --currIdx) : (currIdx+1 > max ? 0 : ++currIdx)).trigger(click);
       };
+
   $node.on(click, 'li', function() {
     var $this = $(this);
     if ($this.hasClass(active)) { return; }
     $this.siblings( ['.',active].join('') ).addBack().toggleClass('active');
     currIdx = $this.index();
+    $node.trigger('slideshow:change', $this);
   }).on(click, '.js_next', function() {
     slide();
   }).on(click, '.js_prev', function() {
     slide(true);
   });
+
+  return {
+    getCurrentSlide: function() {
+      return $items.eq(currIdx);
+    }
+  };
 };
