1fbb265d50a6a5668b5bb5bbf52087d9bae76bf5
Robert Fleischmann <rendro87@gmail.com>
2017-02-20 17:59:53 -0800

    remove src and dist and add babel

--- src/jquery.js
+++
@@ -1,7 +0,0 @@
-$.fn.vintage = function(options, effect) {
-  return this.each(function() {
-    if (!$.data(this, 'vintageJS')) {
-      $.data(this, 'vintageJS', new VintageJS( this, options, effect ));
-    }
-  });
-};
